dc08082abc3b2beb0af9c6b5ec667d07cf2ed315
779208739 <lhf779208739@gmail.com>
2023-04-17 01:32:04 -0700

    Update GameAppend

--- management/GameAppend.java
+++ management/GameAppend.java
@@ -5,58 +5,96 @@ import javax.swing.*;
 import javax.swing.filechooser.FileFilter;
 import javax.swing.filechooser.FileNameExtensionFilter;
 
-import entity.Game;
-
+import java.awt.Image;
+import java.awt.image.BufferedImage;
 import java.awt.Color;
+import java.awt.Component;
+import java.util.ArrayList;
 import java.util.List;
+import java.io.File;
+import java.io.IOException;
 
 public class GameAppend extends JPanel {
+
+    BufferedImage ImgSource;
+    Image Img;
 
     JLabel labelName = new JLabel("Name: ");
     JLabel labelDes = new JLabel("Description: ");
     JLabel labelType = new JLabel("Type: ");
-    JLabel ImgPath = new JLabel("No image selected");
     JLabel labelImg = new JLabel("Image: ");
     JLabel labelKeywords = new JLabel("Keywords: ");
 
     JTextField Name = new JTextField();
     JTextArea Description = new JTextArea();
     JTextField Type = new JTextField();
+    JTextField ImgPath = new JTextField("No image selected");
     JFileChooser fc = new JFileChooser();
-    JButton BtnAddImg = new JButton("Add Image");
+    JButton BtnAddImg = new JButton("+");
     JButton BtnAddKeywords = new JButton("+");
+    JButton BtnSave = new JButton("Save");
 
-    List<String> Keywords;
+    List<JTextField> Keywords = new ArrayList<>();
 
     public void init() {
         this.setBackground(Color.GRAY);
-        this.setSize(600, 500);
+        this.setSize(600, 470);
         this.setLayout(null);
 
-        labelName.setBounds(100, 30, 200, 50);
-        labelDes.setBounds(100, 100, 200, 50);
-        labelType.setBounds(100, 280, 200, 50);
-        labelImg.setBounds(100, 350, 200, 50);
-        labelKeywords.setBounds(100, 420, 200, 50);
+        labelName.setBounds(100, 20, 200, 30);
+        labelDes.setBounds(100, 100, 200, 30);
+        labelType.setBounds(100, 220, 200, 30);
+        labelImg.setBounds(100, 300, 200, 30);
+        labelKeywords.setBounds(100, 380, 200, 30);
 
-        Name.setBounds(280, 30, 200, 50);
-        Description.setBounds(280, 100, 200, 160);
-        Type.setBounds(280, 280, 200, 50);
-        ImgPath.setBounds(280, 350, 200, 50);
-        BtnAddImg.setBounds(180, 350, 80, 50);
-        BtnAddKeywords.setBounds(210, 420, 50, 50);
+        Name.setBounds(100, 50, 300, 40);
+        Description.setBounds(100, 130, 300, 80);
+        Type.setBounds(100, 250, 300, 40);
+        ImgPath.setBounds(100, 330, 300, 40);
+        BtnAddImg.setBounds(40, 335, 40, 30);
+        BtnAddKeywords.setBounds(40, 415, 40, 30);
+        BtnSave.setBounds(420, 400, 160, 50);
+
+        LoadImg("static/pixel_test.png");
 
         BtnAddImg.addActionListener((e) -> {
             getImgPath();
         });
 
         BtnAddKeywords.addActionListener((e) -> {
-            addKeywords();
-            BtnAddKeywords.setLocation(20 + 80 * Keywords.size(), 420);
-
+            JTextField OneKeyword = new JTextField();
+            OneKeyword.setBounds(100 + 75 * Keywords.size(), 410, 70, 40);
+            OneKeyword.setName("keyword");
+            Keywords.add(OneKeyword);
+            this.add(OneKeyword);
+            this.repaint();
             // Size of keywords should less than 4
             if (Keywords.size() > 3)
                 BtnAddKeywords.setEnabled(false);
+        });
+
+        BtnSave.addActionListener((e) -> {
+            // Add Game;
+
+            if (true) {
+                // Successfully add game
+                JOptionPane.showMessageDialog(this, "Success!", "Success", JOptionPane.INFORMATION_MESSAGE);
+
+                Name.setText("");
+                Description.setText("");
+                Type.setText("");
+                ImgPath.setText("No image selected");
+
+                Component[] componentList = this.getComponents();
+                for (Component c : componentList) {
+                    if (c.getName() == "keyword")
+                        this.remove(c);
+                }
+                LoadImg("static/pixel_test.png");
+
+            } else {
+                JOptionPane.showMessageDialog(this, "Add fails!", "Failure", JOptionPane.ERROR_MESSAGE);
+            }
         });
 
         this.add(labelName);
@@ -71,22 +109,47 @@ public class GameAppend extends JPanel {
         this.add(Type);
         this.add(BtnAddImg);
         this.add(BtnAddKeywords);
+        this.add(BtnSave);
 
     }
 
     private void getImgPath() {
         FileFilter imageFilter = new FileNameExtensionFilter("Image Files", ImageIO.getReaderFileSuffixes());
-        fc.addChoosableFileFilter(imageFilter);
 
+        fc.setFileFilter(imageFilter);
         if (fc.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
             ImgPath.setText(fc.getSelectedFile().getAbsolutePath());
+
+            LoadImg(ImgPath.getText());
         }
     }
 
-    private JTextField addKeywords() {
-        JTextField OneKeyword = new JTextField();
-        OneKeyword.setBounds(50 * Keywords.size(), 420, 80, 50);
+    // Load Image after open the image
+    private void LoadImg(String path) {
+        try {
 
-        return OneKeyword;
+            ImgSource = ImageIO.read(new File(path));
+        } catch (IOException ex) {
+            System.out.println(new File(path).getPath());
+            System.out.println(ex);
+        }
+
+        this.Img = ImgSource.getScaledInstance(160, 160, Image.SCALE_DEFAULT);
+        // Change the img
+        // Remove the old img from the panel
+        Component[] componentList = this.getComponents();
+        for (Component c : componentList) {
+            if (c.getName() == "thumbnail")
+                this.remove(c);
+        }
+        // Add new img to the panel
+        JLabel thumbnail = new JLabel(new ImageIcon(Img));
+        thumbnail.setName("thumbnail");
+        thumbnail.setBounds(420, 210, 160, 160);
+        this.add(thumbnail);
+
+        this.revalidate();
+        this.repaint();
     }
+
 }
